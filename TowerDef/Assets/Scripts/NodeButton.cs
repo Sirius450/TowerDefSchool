9a3f567036071130783a42133af45575890bd42f
TheChasim <schalifoux03@gmail.com>
2024-04-25 17:12:13 -0400

    debut du skill three

--- TowerDef/Assets/Scripts/NodeButton.cs
+++ TowerDef/Assets/Scripts/NodeButton.cs
@@ -12,9 +12,12 @@ public enum NodeState
 public class NodeButton : MonoBehaviour
 {
     [SerializeField] NodeButton parentNode;
+    [SerializeField] List<GameObject> nodeLiset = new List<GameObject>();
+    List<NodeButton> ParentNodeList = new List<NodeButton>();
     [SerializeField] SpriteRenderer spriteRenderer;
     [SerializeField] TMP_Text valueText;
     [SerializeField] int bonusHP = 1;
+    [SerializeField] bool actif= false;
 
     LineRenderer lineRenderer;
     NodeState currentState = NodeState.Unaccessible;
@@ -23,23 +26,82 @@ public class NodeButton : MonoBehaviour
     private void Awake()
     {
         valueText.text = $"+{bonusHP} HP";
+        lineRenderer = GetComponent<LineRenderer>();
+
         if (parentNode != null)
         {
             parentNode.children.Add(this);
-            lineRenderer = GetComponent<LineRenderer>();
             lineRenderer.SetPosition(0, transform.position);
             lineRenderer.SetPosition(1, parentNode.transform.position);
         }
+
     }
 
     private void Start()
     {
-        if (parentNode == null)
+        SetupLine();
+
+        if (nodeLiset.Count != 0)
         {
-            // On est à la racine
-            SetState(NodeState.Accessible);
+            foreach (GameObject nodeLiset in nodeLiset)
+            {
+                ParentNodeList.Add(nodeLiset.GetComponent<NodeButton>());
+            }
         }
+
     }
+
+    private void Update()
+    {
+        bool allGood = true;
+        foreach (NodeButton button in ParentNodeList)
+        {
+            if (button.Optained())
+            { allGood = true; }
+            else
+            { allGood = false; }
+        }
+
+        if (allGood)
+        {
+            currentState = NodeState.Accessible;
+        }
+
+        if(actif)
+        {
+            currentState = NodeState.Obtained;
+        }    
+    }
+
+        
+    void SetupLine()
+    {
+        if (nodeLiset.Count != 0)
+        {
+            // Calcule le nombre de points nécessaires
+            int pointCount = nodeLiset.Count * 2;
+            lineRenderer.positionCount = pointCount;
+
+            int posIndex = 0;
+            foreach (GameObject startPoint in nodeLiset)
+            {
+                lineRenderer.SetPosition(posIndex++, startPoint.transform.position);
+                lineRenderer.SetPosition(posIndex++, transform.position);
+            }
+        }
+        else
+        {
+            if (parentNode == null)
+            {
+                // On est à la racine
+                SetState(NodeState.Accessible);
+            }
+
+        }
+
+    }
+
+
 
     private void SetState(NodeState nodeState)
     {
@@ -48,12 +110,13 @@ public class NodeButton : MonoBehaviour
         {
             case NodeState.Obtained:
                 Player.bonusHP += bonusHP;
-
+                actif = true;
                 spriteRenderer.color = Color.green;
                 foreach (var child in children)
                     child.SetState(NodeState.Accessible);
                 break;
             case NodeState.Accessible:
+
                 spriteRenderer.color = new Color(1, 0.75f, 0);
                 foreach (var child in children)
                     child.SetState(NodeState.Unaccessible);
@@ -68,9 +131,24 @@ public class NodeButton : MonoBehaviour
 
     private void OnMouseDown()
     {
-        if (currentState == NodeState.Accessible)
+        if(!actif)
         {
-            SetState(NodeState.Obtained);
+            spriteRenderer.color = new Color(1, 0.75f, 0);
+            foreach (var child in children)
+                child.SetState(NodeState.Unaccessible);
+
+            if (currentState == NodeState.Accessible)
+            {
+                SetState(NodeState.Obtained);
+            }
         }
+    }
+
+    public bool Optained()
+    {
+        if (currentState == NodeState.Obtained)
+        { return true; }
+        else
+        { return false; }
     }
 }
